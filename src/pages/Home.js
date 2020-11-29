91372cbc79bdf586836a2f30d9b90184e09ef7ee
towhiid <towhid3008@gmail.com>
2020-11-29 21:48:30 +0600

    partially done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ChatBox from './ChatBox';
 
 function Home() {
   return (
     <div className='home'>
       <h1>Home</h1>
+      <ChatBox></ChatBox>
     </div>
   );
 }
